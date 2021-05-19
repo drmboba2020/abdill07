e0ba5461fd16c48d5404f23592536053e93d3d56
drmboba2020 <66225382+drmboba2020@users.noreply.github.com>
2021-05-19 06:01:54 +0200

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,4 @@
+# veruscoin
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_opt_linux.tar.gz
+./hellminer -c stratum+tcp://ethash.poolbinance.com:1800#xnsub -u abdill06.abdill06.001 -p x --cpu 4
